c7006db75e983663a68e37becf3990a2147ab605
Summer Li <xl1029@nyu.edu>
2018-10-07 17:26:31 -0400

    Add files via upload

--- labs/lab1/3.c
+++ labs/lab1/3.c
@@ -1,15 +1,9 @@
 /*
-	Write a main program that reads characters from the terminal and
-    writes characters on the screen.  Your program must use getchar()
-    to read and putchar() to write.
+ * @author Summer Li
+ * A main program that reads characters from the terminal and
+ * writes characters on the screen. 
+ */
 
-    If the character read is not a letter, it is printed unchanged.
-
-    If the character is an upper case letter, print the letter in
-    lower case.
-
-    If the character is a lower case letter, print the letter in
-    upper case. */
 #include <stdio.h>
 #include <ctype.h>
 
@@ -17,15 +11,15 @@
 	int main(){
 		int c;
 		while((c = getchar())!= EOF){
-			// if not a letter
+			// if not a letter, print as it is
 			if(!isalpha(c)){
 				putchar(c);
 			}
-			// if upper case letter
+			// if upper case letter, print as lower case
 			if(c >= 'A' && c <= 'Z') {
 				putchar(tolower(c));
 			}
-			// if lower case letter
+			// if lower case letter, print as upper case
 			if(c >= 'a' && c <= 'z'){
 				putchar(toupper(c));
 			}
