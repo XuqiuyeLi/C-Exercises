55b1edf13bef6254e76f1e1b128f88e70c4f1b6c
Summer Li <xl1029@nyu.edu>
2018-10-06 23:17:33 -0400

    Add files via upload

---
+++ labs/lab1/3.c
@@ -0,0 +1,15 @@
+/*
+	Write a main program that reads characters from the terminal and
+    writes characters on the screen.  Your program must use getchar()
+    to read and putchar() to write.
+
+    If the character read is not a letter, it is printed unchanged.
+
+    If the character is an upper case letter, print the letter in
+    lower case.
+
+    If the character is a lower case letter, print the letter in
+    upper case. */
+
+
+	
